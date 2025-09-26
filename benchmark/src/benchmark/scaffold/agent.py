43c0c818a87fab24adde7c697b828e16a966cd5c
Hazel Shackleton <hazel.shackleton@galois.com>
2025-09-26 13:07:23 -0500

    Fist pass at adding lean MCP.

--- benchmark/src/benchmark/scaffold/agent.py
+++ benchmark/src/benchmark/scaffold/agent.py
@@ -1,11 +1,25 @@
 from inspect_ai.solver import basic_agent, solver
+from inspect.ai.tool import mcp_tools
 from generate.scaffold.tool import lean_tool
+from generate.scaffold.task import lean_server
+from agents import HostedMCPTool
 
 
 # file is currently dead code
 @solver
 def lean_agent(max_attempts: int = 5):
     return basic_agent(
-        tools=[lean_tool()],
+        tools=[
+            lean_tool(),
+            mcp_tools(
+                lean_server,
+                # https://github.com/oOo0oOo/lean-lsp-mcp
+                tools=[
+                    "lean_diagnostic_messages",
+                    "lean_completions",
+                    "lean_multi_attempt",
+                ],
+            ),
+        ],
         max_attempts=max_attempts,
     )
